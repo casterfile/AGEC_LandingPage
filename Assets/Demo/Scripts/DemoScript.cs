ae850b75e245eddc85e63ab85b4b917506755871
acastor <anthony@globalmobility.net>
2021-02-15 18:09:45 +0800

    Upload 02

---
+++ Assets/Demo/Scripts/DemoScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityAndroidOpenUrl;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoScript : MonoBehaviour
+{
+    public Text Output;
+    private const string MAIN_DIR = "/storage/emulated/0";
+    private const string testFolderName = "Test Documents";
+    private const string templatePDFName = "FB.apk";
+
+    private static string pathToTestFolder;
+    private static string pathToTemplatePDF;
+
+    private void Start()
+    {
+        Output.text = "Hello word";
+        pathToTestFolder = Path.Combine(MAIN_DIR, testFolderName);
+        if (!Directory.Exists(pathToTestFolder))
+        {
+            Directory.CreateDirectory(pathToTestFolder);
+        }
+
+        StartCoroutine(SaveTemplatePDF());
+    }
+
+    private IEnumerator SaveTemplatePDF()
+    {
+        string localPathToTemplatePDF = Path.Combine(Application.streamingAssetsPath, templatePDFName);
+        WWW www = new WWW(localPathToTemplatePDF);
+        yield return www;
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.LogError("Error while loading template PDF: " + www.error);
+            yield break;
+        }
+        pathToTemplatePDF = Path.Combine(pathToTestFolder, templatePDFName);
+        File.WriteAllBytes(pathToTemplatePDF, www.bytes);
+
+        AndroidOpenUrl.OpenFile(pathToTemplatePDF);
+    }
+}
